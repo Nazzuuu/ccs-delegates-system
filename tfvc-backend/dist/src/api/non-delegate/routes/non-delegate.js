5e7387c9b2d09e029aea929c4db0623d860716da
Nazzu <162360125+Nazzuuu@users.noreply.github.com>
2026-07-31 13:15:30 +0800

    chore: add compiled dist files for non-delegate collection

---
+++ tfvc-backend/dist/src/api/non-delegate/routes/non-delegate.js
@@ -0,0 +1,36 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    routes: [
+        {
+            method: 'GET',
+            path: '/non-delegates',
+            handler: 'non-delegate.find',
+            config: { auth: false },
+        },
+        {
+            method: 'GET',
+            path: '/non-delegates/:id',
+            handler: 'non-delegate.findOne',
+            config: { auth: false },
+        },
+        {
+            method: 'POST',
+            path: '/non-delegates',
+            handler: 'non-delegate.create',
+            config: { auth: false },
+        },
+        {
+            method: 'PUT',
+            path: '/non-delegates/:id',
+            handler: 'non-delegate.update',
+            config: { auth: false },
+        },
+        {
+            method: 'DELETE',
+            path: '/non-delegates/:id',
+            handler: 'non-delegate.delete',
+            config: { auth: false },
+        },
+    ],
+};
